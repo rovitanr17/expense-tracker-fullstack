db3b72dce5b0e961217e0487c4fce0e147f6ec68
rnrm16 <rovita.nuur.ramadhani@ges-thm.de>
2026-01-03 22:26:26 +0100

    fix: sequelize config add dialect

--- expense-tracker-server/config/config.cjs
+++ expense-tracker-server/config/config.cjs
@@ -1,13 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
+
+const base = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 5432),
+  dialect: "postgres",
+  logging: false,
+};
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    dialect: 'postgres',
-    logging: false
-  }
+  development: { ...base },
+  production: { ...base },
 };
